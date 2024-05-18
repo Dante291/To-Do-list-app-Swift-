57541932b08cd2000b2a35e508164d7af4a4c645
Dante291 <gparag222@gmail.com>
2024-05-18 11:06:03 +0530

    Initial Commit

---
+++ veryfirstapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  veryfirstapp
+//
+//  Created by SAKSHI GUPTA on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ veryfirstapp/veryfirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  veryfirstappApp.swift
+//  veryfirstapp
+//
+//  Created by SAKSHI GUPTA on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct veryfirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
